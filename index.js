ee694b0d301f5fb7a5f34c864056ac4a6c732e9b
Nikesh <nikesh.ramnani@gmail.com>
2021-06-11 21:30:56 +0530

    add secondary-aside data dynamically

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//  ADD PRIMARY ASIDE DATA FROM JSON
 const fetchPrimaryAsideData = async () => {
   const jsonPrimaryAside = await fetch("./data/asidePrimary.json");
   let dataPA = await jsonPrimaryAside.json();
@@ -8,17 +9,44 @@ const fetchPrimaryAsideData = async () => {
 
 const addPrimaryAsideItem = (data) => {
   const primaryAsideElement = document.querySelector(".article-primary-items");
-  console.log(data);
+  // console.log(data);
   data.forEach((datum) => {
-    const htmlTag = `<a class="article-primary-item border-top" href="${datum.anchorLink}"><img src="${datum.imgSrc}" alt="${datum.imgAlt}" class="article-primary-item--img" /><p class="article-primary-item--desc text-overflow-2-lines">"${datum.description}"</p></a>`;
-    console.log(htmlTag);
+    const htmlTag = `<a class="article-primary-item border-top" href="${datum.anchorLink}"><img src="${datum.imgSrc}" alt="${datum.imgAlt}" class="article-primary-item--img" /><p class="article-primary-item--desc text-overflow-2-lines">${datum.description}</p></a>`;
+    // console.log(htmlTag);
     primaryAsideElement.innerHTML += htmlTag;
   });
 };
 
 const startPrimaryAside = async () => {
   const primaryAsideData = await fetchPrimaryAsideData();
-  const addPrimaryAsideData = addPrimaryAsideItem(primaryAsideData);
+  addPrimaryAsideItem(primaryAsideData);
+};
+
+//  ADD SECONDARY ASIDE DATA FROM JSON
+const fetchSecondaryAsideData = async () => {
+  const jsonSecondaryAside = await fetch("./data/asideSecondary.json");
+  let dataSA = await jsonSecondaryAside.json();
+  dataSA = dataSA.secondaryAside;
+  // console.log(dataSA);
+  return dataSA;
+};
+
+const addSecondaryAsideItem = (data) => {
+  const secondaryAsideElement = document.querySelector(
+    ".article-secondary-items"
+  );
+  // console.log(data);
+  data.forEach((datum) => {
+    const htmlTag = `<a class="article-secondary-item border-top" href="${datum.anchorLink}"><p class="article-secondary-item--desc">${datum.pDescription}<span>${datum.spanDescription}</span></p><img	src="${datum.imgSrc}"	alt="${datum.imgAlt}" class="article-secondary-item--img"	/></a>`;
+    // console.log(htmlTag);
+    secondaryAsideElement.innerHTML += htmlTag;
+  });
+};
+
+const startSecondaryAside = async () => {
+  const secondaryAsideData = await fetchSecondaryAsideData();
+  addSecondaryAsideItem(secondaryAsideData);
 };
 
 startPrimaryAside();
+startSecondaryAside();
