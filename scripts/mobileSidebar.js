0911be96617e9b9f7a2a318abb244c9c3863cfdc
Nikesh <nikesh.ramnani@gmail.com>
2021-06-17 01:03:42 +0530

    make video-player display above sidebar

--- scripts/mobileSidebar.js
+++ scripts/mobileSidebar.js
@@ -1,9 +1,7 @@
 const toggleSidebar = () => {
   const navItemsElement = document.querySelector(".nav-items");
-  console.log(navItemsElement);
   navItemsElement.classList.toggle("active-sidebar");
 };
 
 const menuElement = document.querySelector(".menu-icon");
 menuElement.addEventListener("click", toggleSidebar);
-console.log(menuElement);
